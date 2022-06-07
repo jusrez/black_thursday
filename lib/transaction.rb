00805ac4793db132bc8c824f3005537a42237ad9
Thiago Silveira <thiago1992@yahoo.com>
2022-06-06 23:41:15 -0600

    Fix: Transaction repo add symbols in CSV foreach
    Co-authored-by: Nick <nickjones9605@gmail.com>
    Co-authored-by: Kevin Ta <kvnta@yahoo.com>

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -5,12 +5,13 @@ class Transaction
               :result,
               :created_at,
               :updated_at
-def initialize(data)
-@id = data[:id]
-@invoice_id = data[:invoice_id]
-@credit_card_number = data[:credit_card_number]
-@result = data[:result]
-@created_at = data[:created_at]
-@updated_at = data[:updated_at]
-end
+
+  def initialize(data)
+    @id = data[:id]
+    @invoice_id = data[:invoice_id]
+    @credit_card_number = data[:credit_card_number]
+    @result = data[:result]
+    @created_at = data[:created_at]
+    @updated_at = data[:updated_at]
+  end
 end
--- lib/transaction_repository.rb
+++ lib/transaction_repository.rb
@@ -1,24 +1,18 @@
 require_relative './transaction'
 require 'CSV'
 class TransactionRepository
-  attr_reader :transaction_path,
-              :all,
-              :invoice_id,
-              :credit_card_number,
-              :result,
-              :created_at,
-              :updated_at
+	attr_reader :all
   def initialize(transaction_path)
     @transaction_path = transaction_path
     @all = []
 
     CSV.foreach(@transaction_path, headers: true, header_converters: :symbol) do |row|
-    @all << Invoice.new({@id => row[:id],
-      @invoice_id => row[:invoice_id],
-      @credit_card_number => row[:credit_card_number],
-      @result => row[:result],
-      @created_at => row[:created_at],
-      @updated_at => row[:updated_at]})
+    @all << Transaction.new({:id => row[:id],
+      :invoice_id => row[:invoice_id],
+      :credit_card_number => row[:credit_card_number],
+      :result => row[:result],
+      :created_at => row[:created_at],
+      :updated_at => row[:updated_at]})
     end
   end
 
@@ -27,4 +21,7 @@ class TransactionRepository
       transaction.id.to_i == id
     end
   end
+
+
+
 end
