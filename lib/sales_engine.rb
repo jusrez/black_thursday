b56cb15bca1032d6490ea714e3ef3c472c6ffe5b
Justin Ramirez <jusrez528@gmail.com>
2022-06-06 23:24:06 -0400

    Create instance variable for customers and added to self.from_csv Co-authored-by: Nick <nickjones9605@gmail.com> Co-authored-by: Thiago <thiago1992@yahoo.com> Co-authored-by: Kevin Ta <kvnta@yahoo.com>

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -2,17 +2,19 @@ class SalesEngine
   attr_reader :items,
               :merchants,
               :invoices,
-              :analyst
+              :analyst,
+							:customers
 
-  def initialize(items_path, merchants_path, invoice_path)
+  def initialize(items_path, merchants_path, invoices_path, customers_path)
     @items      = ItemRepository.new(items_path)
     @merchants  = MerchantRepository.new(merchants_path)
-    @invoices   = InvoiceRepository.new(invoice_path)
+    @invoices   = InvoiceRepository.new(invoices_path)
     @analyst    = SalesAnalyst.new(items, merchants, invoices)
+		@customers  = CustomerRepository.new(customers_path)
   end
 
   def self.from_csv(data)
-    return SalesEngine.new(data[:items], data[:merchants], data[:invoices])
+    return SalesEngine.new(data[:items], data[:merchants], data[:invoices], data[:customers])
   end
 
 end
