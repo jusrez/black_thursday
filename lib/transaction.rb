4a0c762dd8714c05ca9d1f4bfe53b683a287c3ba
Justin Ramirez <jusrez528@gmail.com>
2022-06-07 19:02:11 -0400

    Add credit card expiration date as an attribute

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,15 +1,18 @@
 class Transaction
   attr_reader :id,
               :invoice_id,
-              :credit_card_number,
-              :result,
-              :created_at,
-              :updated_at
+              :created_at
+
+	attr_accessor :credit_card_number,
+								:credit_card_expiration_date,
+								:result,
+								:updated_at
 
   def initialize(data)
     @id = data[:id]
     @invoice_id = data[:invoice_id]
     @credit_card_number = data[:credit_card_number]
+		@credit_card_expiration_date = data[:credit_card_expiration_date]
     @result = data[:result]
     @created_at = data[:created_at]
     @updated_at = data[:updated_at]
