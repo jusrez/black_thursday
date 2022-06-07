17a8e9e372c6b1879449fc6c6ead6f1106e527a1
Justin Ramirez <jusrez528@gmail.com>
2022-06-06 23:19:24 -0400

    Change some attr_readers into attr_accessors

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,5 +1,6 @@
 class Customer
-	attr_reader :id, :first_name, :last_name, :created_at, :updated_at
+	attr_reader :id, :created_at
+	attr_accessor :first_name, :last_name, :updated_at
 	def initialize(data)
 		@id = data[:id]
 		@first_name = data[:first_name]
